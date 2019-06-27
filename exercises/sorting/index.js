a3b81ea17e2a14c9df095c22e9904e712d49c640
Harrison Wu <harrisonywu@gmail.com>
2019-06-26 17:08:57 -0700

    Implement bubbleSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,27 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+  /* 
+  PSEUDOCODE
+  input array
+  output array, sorted
+  constraints none
+  edge case none
 
+  should loop through arr.length # of times
+  in each loop, loop through the array, comparing the current and next number. If current is larger than next, swap
+  
+  */
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
